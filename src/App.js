efc1ddc1725ee4cd368d06996b17f207fa617103
Silas de Paula <silasandrade94@gmail.com>
2021-05-26 17:32:26 -0300

    projeto ceep

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FormularioCadastro from './componets/FormularioCadastro';
+import ListaDeNotas from './componets/listaDeNotas';
+
+
+
+function App() {
+  return (
+   <section>
+
+   <FormularioCadastro/>
+   <ListaDeNotas/>
+   </section>
+  );
+}
+
+export default App;
