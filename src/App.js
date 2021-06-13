355fda23e857da97a3d6468dd086353e8d15ee77
Silas de Paula <silasandrade94@gmail.com>
2021-06-12 23:22:21 -0300

    lista de notas

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
+  
+  constructor(){
+    super();
+    this.notas = [];
+  }
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    this.notas.push(novaNota);
+    console.log(this.notas.length);
+  }
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.notas}/>
       </section>
     );
   }
