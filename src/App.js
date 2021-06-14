a3167b5720eb1cfa24c41031b6505d314223e384
Silas de Paula <silasandrade94@gmail.com>
2021-06-14 09:32:51 -0300

    programa concluido

--- src/App.js
+++ src/App.js
@@ -4,24 +4,33 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
-  
+
   constructor(){
     super();
-    this.notas = [];
+
+    this.state = {
+      notas:[]
+    }
   }
+
   criarNota(titulo, texto){
     const novaNota = {titulo, texto};
-    this.notas.push(novaNota);
-    console.log(this.notas.length);
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
   }
+
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.notas}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -4,29 +4,31 @@ class FormularioCadastro extends Component {
 
   constructor(props){
     super(props);
-    this.titulo = "";
-    this.texto = "";
+    this.titulo ="";
+    this.texto ="";
   }
 
-  _handleMudancaTitulo(event){
-    event.stopPropagation();
-    this.titulo =event.target.value;
-   
+  _handleMudancaTitulo(evento){
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
   }
-  _handleMudancaTexto(event){
-    event.stopPropagation();
-    this.texto =event.target.value; 
+
+  _handleMudancaTexto(evento){
+    evento.stopPropagation();
+    this.texto = evento.target.value;
   }
-  _criarNota(event){
-    event.preventDefault();
-    event.stopPropagation();
+
+  _criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
     this.props.criarNota(this.titulo, this.texto);
+    
   }
 
   render() {
     return (
-      <form className="form-cadastro "
-      onSubmit={this._criarNota.bind(this)}
+      <form className="form-cadastro"
+        onSubmit={this._criarNota.bind(this)}
       >
         <input
           type="text"
